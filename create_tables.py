4e023337bd542b1af013571e5591de70e545986b
Amir Yazdi <amirmohammad12342381@gmail.com>
2026-07-22 11:44:09 +0330

    ignore create_tables.py

--- create_tables.py
+++
@@ -1,9 +0,0 @@
-from app.db.database import Base, engine
-
-from app.models.user import User
-from app.models.product import Product
-from app.models.sales import Sale
-
-Base.metadata.create_all(bind=engine)
-
-print("Tables created successfully.")
